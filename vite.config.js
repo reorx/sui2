5db6250f8e8b3b15d622a630fdbf8b28cebf0f1f
Reorx <novoreorx@gmail.com>
2022-10-01 00:56:18 +0800

    get iconify icons from handlebar

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import handlebars from 'vite-plugin-handlebars'
+import { readFileSync } from 'fs'
+
+const iconsPath = resolve(__dirname, 'node_modules/@iconify-json/mdi/icons.json')
+const iconsData = JSON.parse(readFileSync(iconsPath))
+// console.log(Object.keys(iconsData))
 
 var data
 try {
@@ -13,12 +18,13 @@ try {
   data = await import('./data.example.json')
 }
 
+
 export default defineConfig({
   // use relative path for assets
-  // base: "",
+  base: "",
   build: {
     // put assets in the same folder as index.html
-    // assetsDir: ".",
+    assetsDir: ".",
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
@@ -28,6 +34,14 @@ export default defineConfig({
   plugins: [
     handlebars({
       context: data,
+      helpers: {
+        iconify: (name) => {
+          const icon = iconsData.icons[name]
+          if (!icon) return `no icon ${name}`
+          return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+          ${icon.body}</svg>`
+        }
+      }
     }),
   ],
 })

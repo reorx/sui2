428559be67180d8e9097a6ac35689999ca16b708
Reorx <novoreorx@gmail.com>
2022-10-01 16:05:06 +0800

    use :) as the suffix for greetings

--- js/date.js
+++ js/date.js
@@ -15,16 +15,16 @@ export function greet() {
   let greet = Math.floor(currentTime.getHours() / 6);
   switch (greet) {
     case 0:
-      document.getElementById("header_greet").innerHTML = "Good night!";
+      document.getElementById("header_greet").innerHTML = "Good night :)";
       break;
     case 1:
-      document.getElementById("header_greet").innerHTML = "Good morning!";
+      document.getElementById("header_greet").innerHTML = "Good morning :)";
       break;
     case 2:
-      document.getElementById("header_greet").innerHTML = "Good afternoon!";
+      document.getElementById("header_greet").innerHTML = "Good afternoon :)";
       break;
     case 3:
-      document.getElementById("header_greet").innerHTML = "Good evening!";
+      document.getElementById("header_greet").innerHTML = "Good evening :)";
       break;
   }
 }

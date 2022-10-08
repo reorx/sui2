6d70b3df5c4bdee3b1017dcbcbaafb4436de4d12
Reorx <novoreorx@gmail.com>
2022-10-08 17:22:35 +0800

    log duration for SW events

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ import { bindThemeButtons, loadTheme } from "./themer";
 import { initKeyboardSearch } from "./search"
 import { registerSW } from 'virtual:pwa-register'
 
+const t0 = new Date()
+
 document.addEventListener('DOMContentLoaded', () => {
 
   loadTheme()
@@ -13,11 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const updateSW = registerSW({
     onNeedRefresh() {
+      console.log('onNeedRefresh', `${new Date() - t0}ms`)
       if (window.confirm("Start page has new version, refresh to update?")) {
         updateSW(true)
       }
     },
     onOfflineReady() {
+      console.log('onOfflineReady', `${new Date() - t0}ms`)
       console.log('PWA offline is ready')
     }
   })

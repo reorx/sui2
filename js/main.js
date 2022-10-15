058536cc7344001a272a5f73ffb6ae357322a50f
Reorx <novoreorx@gmail.com>
2022-10-15 16:04:24 +0800

    try to import vite-pwa-plugin virtual module conditionally
    this commit cound not work, even thought NO_PWA is set to trun when building,
    node will always raise error as follow:
    [vite]: Rollup failed to resolve import "virtual:pwa-register" from "js/main.js".
    This is most likely unintended because it can break your application at runtime.
    If you do want to externalize this module explicitly add it to
    `build.rollupOptions.external`
    
    will change to registerType: autoUpdate in the next commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 import { greet, date } from "./date";
 import { bindThemeButtons, loadTheme } from "./themer";
 import { initKeyboardSearch } from "./search"
-import { registerSW } from 'virtual:pwa-register'
 
 const t0 = new Date()
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 
   loadTheme()
   date()
@@ -13,16 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
   bindThemeButtons()
   initKeyboardSearch()
 
-  const updateSW = registerSW({
-    onNeedRefresh() {
-      console.log('onNeedRefresh', `${new Date() - t0}ms`)
-      if (window.confirm("Start page has new version, refresh to update?")) {
-        updateSW(true)
+  if (import.meta.env.NO_PWA) {
+    console.log('no pwa')
+  } else {
+    const pwa = await import('virtual:pwa-register')
+    const updateSW = pwa.registerSW({
+      onNeedRefresh() {
+        console.log('onNeedRefresh', `${new Date() - t0}ms`)
+        if (window.confirm("Start page has new version, refresh to update?")) {
+          updateSW(true)
+        }
+      },
+      onOfflineReady() {
+        console.log('onOfflineReady', `${new Date() - t0}ms`)
+        console.log('PWA offline is ready')
       }
-    },
-    onOfflineReady() {
-      console.log('onOfflineReady', `${new Date() - t0}ms`)
-      console.log('PWA offline is ready')
-    }
-  })
+    })
+  }
 })
